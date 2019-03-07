51d4f27d51431dd563b4fb5e9df645ea9f4ed55c
Andy Brody <andrew.brody@gsa.gov>
2019-03-07 12:16:36 -0500

    Use fetch to improve safety of metadata methods.
    When attempting to discover the region or account ID using the EC2
    metadata service, use fetch to raise KeyError if the instance identity
    document doesn't contain the required keys.
    
    These methods should never return nil.

--- lib/login_gov/hostdata/ec2.rb
+++ lib/login_gov/hostdata/ec2.rb
@@ -2,7 +2,10 @@ require 'net/http'
 
 module LoginGov
   module Hostdata
+    # Class to wrap accessing the EC2 metadata service
     class EC2
+      # Standard initializer
+      #
       # @return [EC2]
       def self.load
         response = http.get('/2016-09-02/dynamic/instance-identity/document')
@@ -20,16 +23,21 @@ module LoginGov
 
       attr_reader :document
 
+      # @param [Hash] document An instance identity document parsed from JSON
+      #   returned by the EC2 metadata service at
+      #   http://169.254.169.254/2016-09-02/dynamic/instance-identity/document
       def initialize(document)
         @document = document
       end
 
+      # @return [String] Current EC2 region
       def region
-        document['region']
+        document.fetch('region')
       end
 
+      # @return [String] Current AWS account ID
       def account_id
-        document['accountId']
+        document.fetch('accountId')
       end
     end
   end
