4befdedff1948c6d9201701ba7b460676b3b2b9e
Zach Margolis <zachary.margolis@gsa.gov>
2024-04-19 13:14:50 -0700

    Remove legacy LoginGov namespace

--- lib/login_gov/hostdata.rb
+++
@@ -1,6 +0,0 @@
-require 'identity/hostdata'
-
-# Create an alias with the old LoginGov namespace so we don't break some of our configs
-module LoginGov
-  Hostdata = ::Identity::Hostdata
-end
--- spec/identity/login_gov/hostdata_spec.rb
+++
@@ -1,8 +0,0 @@
-RSpec.describe 'backwards-compatible name' do
-  it 'exists' do
-    expect do
-      require 'login_gov/hostdata'
-      LoginGov::Hostdata.env
-    end.to_not raise_error
-  end
-end
