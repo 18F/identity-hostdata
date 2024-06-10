c7b63fa96611276d6a88dc3022f6981f3e93978d
Zach Margolis <zachary.margolis@gsa.gov>
2024-06-10 09:04:00 -0700

    Refactor EC2 in specs to use shared EC2Helpers module

--- spec/identity/hostdata/config_reader_spec.rb
+++ spec/identity/hostdata/config_reader_spec.rb
@@ -45,22 +45,13 @@ RSpec.describe Identity::Hostdata::ConfigReader do
         'int/idp/v1/worker.yml' => ROLE_YAML,
       }
     end
-    let(:ec2_api_token) { SecureRandom.hex }
 
     before do
       allow(Identity::Hostdata).to receive(:in_datacenter?).and_return(true)
       allow(Identity::Hostdata).to receive(:instance_role).and_return('idp')
       allow(Identity::Hostdata).to receive(:env).and_return('int')
 
-      stub_request(:put, 'http://169.254.169.254/latest/api/token').
-          with(headers: { 'X-Aws-Ec2-Metadata-Token-Ttl-Seconds' => '60' }).
-          to_return(body: ec2_api_token)
-      stub_request(:get, 'http://169.254.169.254/2016-09-02/dynamic/instance-identity/document').
-          with(headers: { 'X-aws-ec2-metadata-token' => ec2_api_token }).
-          to_return(body: {
-            'region' => 'us-west-1',
-            'accountId' => '12345',
-          }.to_json)
+      stub_ec2_metadata
 
       s3_client.stub_responses(
         :get_object, proc do |context|
--- spec/identity/hostdata/ec2_spec.rb
+++ spec/identity/hostdata/ec2_spec.rb
@@ -4,21 +4,16 @@ RSpec.describe Identity::Hostdata::EC2 do
   describe '.load' do
     subject(:load) { Identity::Hostdata::EC2.load }
 
-    let(:ec2_api_token) { SecureRandom.hex }
-
     it 'loads data from the magic ECS URL' do
-      stub_request(:put, 'http://169.254.169.254/latest/api/token').
-          with(headers: { 'X-Aws-Ec2-Metadata-Token-Ttl-Seconds' => '60' }).
-          to_return(body: ec2_api_token)
-      stub_request(:get, 'http://169.254.169.254/2016-09-02/dynamic/instance-identity/document').
-        with(headers: { 'X-aws-ec2-metadata-token' => ec2_api_token }).
-        to_return(body: document.to_json)
+      stub_ec2_metadata(document: document)
 
       ec2 = load
       expect(ec2.region).to eq('us-west-2')
     end
 
     it 'blows up when the request times out' do
+      ec2_api_token = SecureRandom.hex
+
       stub_request(:put, 'http://169.254.169.254/latest/api/token').
           with(headers: { 'X-Aws-Ec2-Metadata-Token-Ttl-Seconds' => '60' }).
           to_return(body: ec2_api_token)
